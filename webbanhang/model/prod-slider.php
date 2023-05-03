3b0743b1ea266e656e5f4bca64f1c83c4ce89567
TrHau03 <haunhoooo@gmail.com>
2023-05-04 00:07:41 +0700

    About us

--- webbanhang/model/prod-slider.php
+++ webbanhang/model/prod-slider.php
@@ -1,16 +1,20 @@
+<?php
+    $div_id = "myDiv";
+?>
 <div class="prod-slider">
     <div class="slider-header">
         <h4>You Might Also Like</h4>
         <div class="action">
-        <button aria-label="Previous" class="glider-prev act-but"><i class="fa-solid fa-chevron-left"></i></button>
+        <button aria-label="Previous" class="glider-prev act-but" style = ".slider:active{padding: 20px}"><i class="fa-solid fa-chevron-left"></i></button>
         <button aria-label="Next" class="glider-next act-but"><i class="fa-solid fa-chevron-right"></i></button>
         </div>
     </div>
         <?php
+            
             $result = mysqli_query($conn, "select * from sanpham where LoaiSP='$LoaiSP'");
             if (mysqli_num_rows($result) > 0) {
                 $s ="";
-                $s.='<div class="slider">';
+                $s.='<div class="slider" id="<?php echo $div_id; ?>">';
                 while($row = mysqli_fetch_assoc($result)){
                     $s .=sprintf('<a href="product-detail.php?ID=%s"><div class="product"><figure class="prod-card">',$row['ID']);
                     $s .=sprintf('<img src="%s">',$row['HinhSP']);
@@ -28,6 +32,11 @@
 </div>
 <link rel="stylesheet" type="text/css" href="../assets/scri/Glider.js-master/glider.js">
 <script>
+       
+
+    $(".right-slider").click(function(){
+        $('#admin-menu').animate({width:'show'},1000);
+    });
     window.addEventListener('load', function(){
         new Glider(document.querySelector('.slider'), {
     slidesToShow: 5,
@@ -40,3 +49,10 @@
 });
 })
 </script>
+<script type="text/javascript">
+		window.onload = function() {
+			var div = document.getElementById("<?php echo $div_id; ?>");
+			div.style.left = "100px";
+			div.style.top = "100px";
+		}
+	</script>
